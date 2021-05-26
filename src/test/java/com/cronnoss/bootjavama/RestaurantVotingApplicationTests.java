f03fc02dc3e54f8cf4e0df9af24a23d9cc13a1b3
Evgeniy Karasev <karasevevg@gmail.com>
2021-05-26 09:43:20 +0300

    first commit

---
+++ src/test/java/com/cronnoss/bootjavama/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cronnoss.bootjavama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
