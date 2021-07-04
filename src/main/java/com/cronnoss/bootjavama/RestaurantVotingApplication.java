3c9a4ebb1b7d62f31e4411e701d46f3aa14cd94d
Evgeniy Karasev <karasevevg@gmail.com>
2021-07-04 18:30:23 +0300

    2_02_h2_init

--- src/main/java/com/cronnoss/bootjavama/RestaurantVotingApplication.java
+++ src/main/java/com/cronnoss/bootjavama/RestaurantVotingApplication.java
@@ -1,15 +1,11 @@
 package com.cronnoss.bootjavama;
 
-import com.cronnoss.bootjavama.model.Role;
-import com.cronnoss.bootjavama.model.User;
 import com.cronnoss.bootjavama.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Set;
 
 @SpringBootApplication
 @AllArgsConstructor
@@ -22,8 +18,6 @@ public class RestaurantVotingApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", Set.of(Role.ROLE_USER)));
-        userRepository.save(new User("admin@cronnoss.com", "Admin_First", "Admin_Last", "admin", Set.of(Role.ROLE_USER, Role.ROLE_ADMIN)));
         System.out.println("userRepository.findAll(): " + userRepository.findAll());
     }
 }
