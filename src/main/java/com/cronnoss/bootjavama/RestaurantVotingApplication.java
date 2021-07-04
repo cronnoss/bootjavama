12eef7ef85442953b7d86bd48c323a9fc0b5ffe7
Evgeniy Karasev <karasevevg@gmail.com>
2021-07-04 21:34:06 +0300

    2_03_model_query

--- src/main/java/com/cronnoss/bootjavama/RestaurantVotingApplication.java
+++ src/main/java/com/cronnoss/bootjavama/RestaurantVotingApplication.java
@@ -18,6 +18,6 @@ public class RestaurantVotingApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        System.out.println("userRepository.findAll(): " + userRepository.findAll());
+        System.out.println("userRepository.findByLastNameContainingIgnoreCase('last'): " + userRepository.findByLastNameContainingIgnoreCase("last"));
     }
 }
