4aead95a7b4adb7f181e8ba340c993a7208d1350
Evgeniy Karasev <karasevevg@gmail.com>
2021-07-08 21:58:37 +0300

    4_05_role_auth

--- src/main/java/com/cronnoss/bootjavama/RestaurantVotingApplication.java
+++ src/main/java/com/cronnoss/bootjavama/RestaurantVotingApplication.java
@@ -1,23 +1,14 @@
 package com.cronnoss.bootjavama;
 
-import com.cronnoss.bootjavama.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @AllArgsConstructor
-public class RestaurantVotingApplication implements ApplicationRunner {
-    private final UserRepository userRepository;
+public class RestaurantVotingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) {
-        System.out.println("userRepository.findByLastNameContainingIgnoreCase('last'): " + userRepository.findByLastNameContainingIgnoreCase("last"));
     }
 }
--- src/main/java/com/cronnoss/bootjavama/config/WebSecurityConfig.java
+++ src/main/java/com/cronnoss/bootjavama/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.cronnoss.bootjavama.config;
 
 import com.cronnoss.bootjavama.AuthUser;
+import com.cronnoss.bootjavama.model.Role;
 import com.cronnoss.bootjavama.model.User;
 import com.cronnoss.bootjavama.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,5 +41,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService())
                 .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/account").hasRole(Role.USER.name())
+                .antMatchers("/api/**").hasRole(Role.ADMIN.name())
+                .and().formLogin();
     }
 }
